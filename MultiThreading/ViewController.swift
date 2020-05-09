063650b3524ecfb92dd08d1f3de89566140524cc
Alireza <aalirezamoradii@gmail.com>
2020-05-09 22:24:42 +0430

    Initial Commit

---
+++ MultiThreading/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MultiThreading
+//
+//  Created by Alireza Moradi on 2/20/1399 AP.
+//  Copyright © 1399 Alireza Moradi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
